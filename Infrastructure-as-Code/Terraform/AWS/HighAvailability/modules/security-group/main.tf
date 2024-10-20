5ef5c38989f60dbf01876aa73dbf824f48160a64
JustinKQuijanoo <justinkquijano@gmail.com>
2024-10-20 15:33:48 -0400

    Security group to allow SSH and HTTP

--- Infrastructure-as-Code/Terraform/AWS/HighAvailability/modules/security-group/main.tf
+++ Infrastructure-as-Code/Terraform/AWS/HighAvailability/modules/security-group/main.tf
@@ -4,30 +4,30 @@
 
 # Security group to allow SSH and HTTP
 resource "aws_security_group" "SG-Hydrohomie" {
-  name = "SG-Hydrohomie"
+  name        = "SG-Hydrohomie"
   description = "Allow SSH and HTTP"
-  vpc_id = var.vpc_id
+  vpc_id      = var.vpc_id
   ingress {
     description = "Allow SSH"
-    from_port = 22
-    to_port = 22
-    protocol = "TCP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "Allow HTTP"
-    from_port = 80
-    to_port = 80
-    protocol = "TCP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     description = "Allow everything outbound"
-    from_port = 0 
-    to_port = 0 
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
